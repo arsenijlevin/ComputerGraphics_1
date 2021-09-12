d3ea4f5e45cadb89368c1e82ebf8623c076e6f42
arsenijlevin <arsenij.levin19@gmail.com>
2021-09-12 03:25:09 +0300

    Initial commit

---
+++ src/ru/vsu/computergraphics/levin/DrawingPanel.java
@@ -0,0 +1,49 @@
+package ru.vsu.computergraphics.levin;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DrawingPanel extends JFrame implements ActionListener {
+  private final int BACKGROUND_WIDTH = 800;
+  private final int BACKGROUND_HEIGHT = 800;
+  private final int TIMER_DELAY = 1000;
+  private final Timer timer = new Timer(TIMER_DELAY, this);
+  private int ticksFromStart = 0;
+
+  public DrawingPanel() {
+    setTitle("Drawing Panel");
+    setSize(BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+    setVisible(true);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    timer.start();
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g;
+
+    g2d.setColor(Color.WHITE);
+    g2d.fillRect(0, 0, BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+
+    g2d.setColor(Color.BLACK);
+
+    g2d.fillRect(
+        ticksFromStart, BACKGROUND_HEIGHT / 2, BACKGROUND_WIDTH / 10, BACKGROUND_WIDTH / 10);
+    g2d.setColor(Color.GREEN);
+    g2d.fillOval(
+        ticksFromStart, BACKGROUND_HEIGHT / 2, BACKGROUND_HEIGHT / 10, BACKGROUND_WIDTH / 10);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == timer) {
+      repaint();
+      ++ticksFromStart;
+    }
+  }
+}
